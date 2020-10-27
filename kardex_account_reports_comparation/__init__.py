dfb69d236ff6f0cb290d8af7965782a0b51b869f
Leoz tmp PC <astaroth.satania@gmail.com>
2020-10-27 09:03:47 -0500

    [ADD] hotel management module

--- kardex_account_reports_comparation/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- encoding: utf-8 -*-
-import kardex_account_report
-import export_file
--- kardex_account_reports_comparation/export_file.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api
-
-class CustomExportFile(models.TransientModel):
-	_name = 'custom.export.file'	
-	
-	output_name = fields.Char('Output filename', size=128)
-	output_file = fields.Binary('Output file', readonly=True, filename="output_name")
